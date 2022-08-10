8a06517e6aac6ef095e595c28aa7fcf16fa6c0e6
fahatmah <fahatmahmabang9@gmail.com>
2022-08-10 21:25:42 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const notifyButton = document.getElementById('notifyButton');
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault();
+	emailInputCheck();
+});
+
+function emailInputCheck() {
+	const emailVal = email.value.trim();
+
+	if (emailVal == '') {
+		invalidEmail(email, 'Enter your email first');
+		notifyButton.classList.add('invalidEmail')
+		notifyButton.classList.remove('validEmail');
+	} else if (!isEmail(emailVal)) {
+		invalidEmail(email, 'Please provide a valid email address');
+		notifyButton.classList.add('invalidEmail');
+		notifyButton.classList.remove('validEmail');
+	} else {
+		notifyButton.classList.add('validEmail');
+		validEmail(email);
+	}
+}
+
+function invalidEmail(input, message) {
+	const emailControl = input.parentElement;
+	const invalidEmailInput = emailControl.querySelector('.validation__txt');
+
+	invalidEmailInput.innerHTML = message;
+
+	emailControl.className = 'email__container invalid';
+}
+
+function validEmail(input) {
+	const emailControl = input.parentElement;
+
+	emailControl.className = 'email__container valid';
+}
+
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+		email
+	);
+}
